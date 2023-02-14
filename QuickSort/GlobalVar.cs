80052b46281a7c5899b083af8b449f6b04d5693e
arutofu <101963349+arutofu@users.noreply.github.com>
2023-02-15 01:35:00 +1100

    Add files via upload

---
+++ QuickSort/GlobalVar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class GlobalVar
+    {
+        public static int count = 0;
+    }
+}
