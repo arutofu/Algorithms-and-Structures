b941b03629e172339a0bcaa87f3508d5abce17f1
arutofu <101963349+arutofu@users.noreply.github.com>
2023-02-15 17:54:37 +1100

    Add files via upload

---
+++ Merge-Sort-Csharp/GlobalVar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Двухпут.слияние
+{
+    class GlobalVar
+    {
+        public static int count = 0;
+    }
+}
