03002c16fb74bd6fd28a676ef8264c29ffe7aba5
arutofu <101963349+arutofu@users.noreply.github.com>
2023-02-15 17:46:55 +1100

    Add files via upload

---
+++ Graph/GraphEdge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class GraphEdge
+    {
+        // Связанная вершина
+        public GraphVertex ConnectedVertex { get; }
+
+        // Вес ребра
+        // public int EdgeWeight { get; }
+
+        // Конструктор
+        /// <param name="connectedVertex">Связанная вершина</param>
+        public GraphEdge(GraphVertex connectedVertex)
+        {
+            ConnectedVertex = connectedVertex;
+        }
+    }
+}
---
+++ Graph/GraphVertex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class GraphVertex
+    {
+        // Название вершины
+        public string Name { get; }
+
+        // Список ребер
+        public List<GraphEdge> Edges { get; }
+
+        // Конструктор
+        /// <param name="vertexName">Название вершины</param>
+        public GraphVertex(string vertexName)
+        {
+            Name = vertexName;
+            Edges = new List<GraphEdge>();
+        }
+
+        // Добавить ребро
+        /// <param name="newEdge">Ребро</param>
+        public void AddEdge(GraphEdge newEdge)
+        {
+            Edges.Add(newEdge);
+        }
+
+        // Добавить ребро
+        /// <param name="vertex">Вершина</param>
+        public void AddEdge(GraphVertex vertex)
+        {
+            AddEdge(new GraphEdge(vertex));
+        }
+
+        public void DelEdge(GraphEdge edge)
+        {
+            Edges.Remove(edge);
+        }
+
+        public void DeleteEdge(GraphVertex vert)
+        {
+            DelEdge(new GraphEdge(vert));
+        }
+    }
+}
